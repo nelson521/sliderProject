c356cb5ddeb775483a1808d885b2646c508f14b7
Stevenson Nelson <sonson521@gmail.com>
2021-01-11 15:45:46 -0500

    add new photos and add if statement.

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const images = [
-  'corina-ardeleanu-sWIxCweDzzs-unsplash.jpg',
-  'sergey-shmidt-koy6FICCy5s-unsplash.jpg',
-  'sharon-mccutcheon-SavQfLRm4Do-unsplash.jpg',
-  'timp-vijn-yjiZAdTDkik-unsplash.jpg',
+let img = [
+  'aaron-burden-GVnUVP8cs1o-unsplash.jpg',
+  'annie-spratt-WBpr_yH0Frg-unsplash.jpg',
+  'gerome-bruneau-RPmWEtZLh7U-unsplash.jpg',
 ];
-
-const btnLeft = document
-  .querySelector('.btn-left')
-  .addEventListener('click', leftSide);
-
-const btnRight = document
-  .querySelector('.btn-right')
-  .addEventListener('click', rightSide);
 
 let count = 0;
 
-const picture = document.querySelector('img');
+const buttonRight = document.querySelector('.btn-right');
+const buttonLeft = document.querySelector('.btn-left');
 
-function leftSide(e) {
+buttonLeft.addEventListener('click', leftClick);
+buttonRight.addEventListener('click', rightClick);
+
+function leftClick(e) {
   if (e.target.parentElement.classList.contains('btn-left')) {
-    console.log('Left');
+    document.querySelector('.flowers').src = `../images/${img[count--]}`;
+    if (count < 0) {
+      count = img.length - 1;
+    }
   }
+
+  console.log(e.target);
+  e.preventDefault();
 }
 
-function rightSide(e) {
+function rightClick(e) {
   if (e.target.parentElement.classList.contains('btn-right')) {
-    console.log('Right');
+    document.querySelector('.flowers').src = `../images/${img[count++]}`;
+    if (count > img.length - 1) {
+      count = 0;
+    }
   }
+  console.log('Right');
+  e.preventDefault();
 }
+console.log(buttonRight);
+console.log(buttonLeft);
